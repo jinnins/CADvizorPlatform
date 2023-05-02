dcbd4480b61b3e96c8d88be2365317dfa83209c5
jimin <gmlwo7819@gmail.com>
2023-05-02 14:28:56 +0900

    first commit

---
+++ CADvizorMain/Views/CADvizorPlatform.xaml.cs
@@ -0,0 +1,73 @@
+﻿using CADvizorMain.Views.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CADvizorMain.Views
+{
+    /// <summary>
+    /// CADvizorPlatform.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CADvizorPlatform : Window
+    {
+        public CADvizorPlatform()
+        {
+            InitializeComponent();
+        }
+        private void border1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+            e.Handled = true;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Views/Pages/HomePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdSounds_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/Pages/Dashboard.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdNotes_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/Pages/NotePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdPayment_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/Pages/Setting.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
