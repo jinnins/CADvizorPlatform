3de26ecf9119f5f87e7cc6ba69b4c0192a1bf33d
jimin <gmlwo7819@gmail.com>
2023-05-04 08:33:34 +0900

    HomePage에 News추가

--- CADvizorMain/Properties/Resources.Designer.cs
+++ CADvizorMain/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace CADvizorMain.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap CADVizorMFG {
+            get {
+                object obj = ResourceManager.GetObject("CADVizorMFG", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- CADvizorMain/Views/Pages/HomePage.xaml.cs
+++ CADvizorMain/Views/Pages/HomePage.xaml.cs
@@ -23,11 +23,33 @@ namespace CADvizorMain.Views.Pages
         public HomePage()
         {
             InitializeComponent();
+
+            news1.MouseLeftButtonDown += news1_MouseLeftButtonDown;
+            news2.MouseLeftButtonDown += News2_MouseLeftButtonDown;
+            news3.MouseLeftButtonDown += News3_MouseLeftButtonDown;
+            news4.MouseLeftButtonDown += News4_MouseLeftButtonDown;
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void News4_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.newswire.co.kr/newsRead.php?no=959908");
+        }
+
+        private void News3_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.newswire.co.kr/newsRead.php?no=964002");
+        }
+
+        private void News2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.newswire.co.kr/newsRead.php?no=964780");
+        }
+
+        private void news1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             System.Diagnostics.Process.Start("https://www.newswire.co.kr/newsRead.php?no=962766");
         }
+
     }
 }
